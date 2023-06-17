ddff0319d5662fca0bf73932a65320f0aeb686df
sethu-ravichandran <rsethu1997@gmail.com>
2023-06-17 14:48:03 +0530

    Final commit.

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = 3500
-const booksRouter = require('./routes/patientsRouter')
-const validateBooksRouter = require('./routes/validatePatientsRouter')
+const patientsRouter = require('./routes/patientsRouter')
+const validatePatientsRouter = require('./routes/validatePatientsRouter')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
@@ -15,7 +15,7 @@ const db = mongoose.connection
 db.on('error', (errorMessage) => console.log(errorMessage))
 db.once('open', () => console.log('Connected successfully to the database..'))
 
-app.use('/api/v1/patients', booksRouter)
-app.use('/api/v1/patients/validate', validateBooksRouter)
+app.use('/api/v1/patients', patientsRouter)
+app.use('/api/v1/patients/validate', validatePatientsRouter)
 
 app.listen(PORT, console.log(`Server listening at http://localhost:${PORT}/api/v1/patients`))
